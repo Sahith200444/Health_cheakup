a50f8a3c79a8b04cc942bd269e16ccfa8d5af7f0
M SAHITH REDDY <22f3001652@ds.study.iitm.ac.in>
2026-01-31 15:07:50 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,23 @@
-console.log("Health Assistant Loaded");
+console.log("AI Health Assistant Ready");
+
+const form = document.querySelector("form");
+const loader = document.getElementById("loader");
+const progressText = document.getElementById("progress");
+
+form.addEventListener("submit", function(){
+
+  loader.style.display="flex";
+
+  let percent=0;
+
+  const interval=setInterval(()=>{
+    if(percent<95){
+      percent+=5;
+      progressText.innerText = percent + "%";
+    }
+  },200);
+
+  // Voice Feedback
+  const msg = new SpeechSynthesisUtterance("Analyzing your health. Please wait.");
+  speechSynthesis.speak(msg);
+});
